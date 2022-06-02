ac4b96c6ffe8a3d215ffb2fbc289e94fc8a3a953
JavierChvez <javierchavez0080@gmail.com>
2022-06-02 09:49:17 -0500

    “JavierChvez”

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,8 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+
+
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
@@ -33,6 +35,8 @@
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
                         @endif
                     @endauth
+
+                        <a href="{{url("chavez")}}" class="text-sm text-gray-700 underline">Chávez</a>
                 </div>
             @endif
 
