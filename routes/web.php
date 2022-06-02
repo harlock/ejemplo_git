eb2aaae4c205fb843260e50a1e85faa46e964cc4
NoonLu <lusifernandors@gmail.com>
2022-06-02 10:59:42 -0500

    LuisF

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Middleware\CheckUser;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LuisController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -36,3 +37,5 @@ Route::middleware([checkUser::class])->group(function(){
 });
 
 Route::resource("users",UserController::class);
+
+Route::get ("luis",[LuisController::class,"index"])->name("luis.index");
